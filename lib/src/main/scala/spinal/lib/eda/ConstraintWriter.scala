15dbb17304216f87e51dd24941de3b784d08dfd1
Pengcheng Xu <pengxu@ethz.ch>
2024-02-01 12:56:18 +0100

    debug clock

--- lib/src/main/scala/spinal/lib/eda/ConstraintWriter.scala
+++ lib/src/main/scala/spinal/lib/eda/ConstraintWriter.scala
@@ -70,7 +70,9 @@ object ConstraintWriter {
 
   def findClockPeriod(cd: ClockDomain, destVar: String = "clk_period"): String =
     s"""
-       |set clk [get_clocks -of [get_nets -hier -filter {NAME =~ */${cd.toString}}]]
+       |set clk_net [get_nets -hier -filter {NAME =~ */${cd.toString}}]
+       |puts "Clock nets: $$clk_net"
+       |set clk [get_clocks -of $$clk_net]
        |set $destVar [get_property -min PERIOD $$clk]
        |""".stripMargin
 
